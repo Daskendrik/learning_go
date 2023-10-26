113801c5427b591c7f465c9da4ab19b3b30c55c9
Daskendrik <ss599@yandex.ru>
2023-10-26 18:06:09 +0300

    закомитила

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	var message string = "Лалала" //строковое число
-	var number int = 45 //целое число
-	var halfnumber float32 = 45.4 //долюное число
-	fmt.Println(message)
-	fmt.Println(number)
-	fmt.Println(halfnumber)
+	// var message string = "Лалала" 	//строковое число
+	// var number int = 45 			//целое число
+	// var halfnumber float32 = 45.4 	//долюное число
+	// var b bool = true 				//логическое
+
+	
 }
