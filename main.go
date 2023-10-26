f590b81255930ef87ae4fbc07fe385822895d1c1
Daskendrik <ss599@yandex.ru>
2023-10-26 18:02:52 +0300

    переменные

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(("Лалала"))
+
+	var message string = "Лалала" //строковое число
+	var number int = 45 //целое число
+	var halfnumber float32 = 45.4 //долюное число
+	fmt.Println(message)
+	fmt.Println(number)
+	fmt.Println(halfnumber)
 }
