687e44c72242c84cf1968c0eac06d3d621af90eb
Dashka_W <Daskendrik@users.noreply.github.com>
2023-10-27 14:23:28 +0300

    fmt.Sprintf

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	// var message string = "Лалала" 	//строковое число
@@ -7,5 +9,17 @@ func main() {
 	// var halfnumber float32 = 45.4 	//долюное число
 	// var b bool = true 				//логическое
 
+	// a, b, c := 1, 2, 3 // множественное присвоение
+
+	// fmt.Println(a, b, c)
+	c := summ(5, 6)
+	mes := fmt.Sprintf("Сумаа 5 и 6 равна %d", c ) // чтобы объединить в строку число и строку
+	fmt.Println(mes)
+	fmt.Println("Заглушка")
 	
+}
+
+func summ(num_1 int, num_2 int) int {
+	sum := num_1 + num_2
+	return sum
 }
