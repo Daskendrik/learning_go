60fe2815bc5cfab0a2465ce404650798a0484ac9
Dashka_W <Daskendrik@users.noreply.github.com>
2023-10-30 13:18:21 +0300

    функция с 2 аутпутами

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
@@ -16,10 +18,22 @@ func main() {
 	mes := fmt.Sprintf("Сумаа 5 и 6 равна %d", c ) // чтобы объединить в строку число и строку
 	fmt.Println(mes)
 	fmt.Println("Заглушка")
+	fmt.Println("   ")
+	message, entered := checkTheClub(15)
+	fmt.Println(message)
+	fmt.Println(entered)
 	
 }
 
 func summ(num_1 int, num_2 int) int {
 	sum := num_1 + num_2
 	return sum
+}
+
+func checkTheClub(age int)(string, bool) {
+	if (age >= 18) {
+		return "Входи",  true
+	} else {
+		return "Вход запрещен", false
+	}
 }
