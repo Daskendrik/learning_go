aa1f0d29f6414801d638ce5fc513356a44dbcf7b
Dasha <52924734+Daskendrik@users.noreply.github.com>
2023-11-04 12:39:37 +0300

    add init

--- main.go
+++ main.go
@@ -4,9 +4,16 @@ import (
 	"errors"
 	"fmt"
 )
+var msg string
+
+func init(){
+	msg="Вызовется перед мэин"
+}
+
+
 
 func main() {
-
+	fmt.Println(msg)
 	// var message string = "Лалала" 	//строковое число
 	// var number int = 45 			//целое число
 	// var halfnumber float32 = 45.4 	//долюное число
